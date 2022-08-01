c03e782bb1a44231ab611a7ae6f27485087137dc
Zahid Hasan <megamind.d.evilll@gmail.com>
2022-08-01 18:40:44 +0600

    Initial Commit

---
+++ Tut_5_SwiftUI_ViewBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tut_5_SwiftUI_ViewBuilder
+//
+//  Created by ZEUS on 1/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tut_5_SwiftUI_ViewBuilder/Tut_5_SwiftUI_ViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut_5_SwiftUI_ViewBuilderApp.swift
+//  Tut_5_SwiftUI_ViewBuilder
+//
+//  Created by ZEUS on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Tut_5_SwiftUI_ViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
